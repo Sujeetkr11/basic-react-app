e2c522c9fad112340f400bcf135e040438f0ff4e
devappdigital <devapp.digital@gmail.com>
2024-06-16 18:46:37 -0500

    created the Page as component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-function Page() {
-  return ( //make sure to add this
-  <div>
-    <h1>My Awesome React Web App</h1>
-    <h3>Reasons I Love React</h3>
-    <ol>
-      <li>It's composable.</li>
-      <li>It's declarative.</li>
-      <li>It's a hireable skill.</li>
-      <li>It's actively maintained by skilled people.</li>
-    </ol>
-  </div>
-)}
+import Page from './components/Page';  //import the Page
 
 function App() {
   return (
     <div className="App">
-      <Page />
+      <Page />    {/* add the page  */}
     </div>
   );
 }
---
+++ src/components/Page.js
@@ -0,0 +1,19 @@
+// Page.js
+import React from 'react';
+
+function Page() {
+  return (
+    <div>
+      <h1>My Awesome React Web App</h1>
+      <h3>Reasons I Love React</h3>
+      <ol>
+        <li>It's composable.</li>
+        <li>It's declarative.</li>
+        <li>It's a hireable skill.</li>
+        <li>It's actively maintained by skilled people.</li>
+      </ol>
+    </div>
+  );
+}
+
+export default Page;
