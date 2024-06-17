79e0b844500eaf41356ba52ca74d9c6586381fd1
devappdigital <devapp.digital@gmail.com>
2024-06-16 20:43:15 -0500

    updated with classname in styles.css and updated navbar.js

--- src/components/Navbar/NavbarComponent.js
+++ src/components/Navbar/NavbarComponent.js
@@ -5,8 +5,8 @@ import './styles.css';
 function NavbarComponent() {
     return (
         <header className="App-header">
-            <nav>
-                <img src="./logo192.png" width="40px" />
+            <nav className="nav">
+                <img src="./logo192.png" />
                 <ul>
                     <li>Pricing</li>
                     <li>About</li>
